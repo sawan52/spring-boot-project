d5ba06f9a3b42a36b5adbb6ad3854c05792cd09a
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-08 18:59:19 +0530

    tested CRUD operations

--- src/test/java/com/sawan/student/dal/StudentdalApplicationTests.java
+++ src/test/java/com/sawan/student/dal/StudentdalApplicationTests.java
@@ -1,13 +1,44 @@
 package com.sawan.student.dal;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sawan.student.dal.entities.Student;
+import com.sawan.student.dal.repos.StudentRepository;
 
 @SpringBootTest
 class StudentdalApplicationTests {
 
+	@Autowired
+	private StudentRepository repo;
+
 	@Test
-	void contextLoads() {
+	void testCreateStudent() {
+		Student student = new Student();
+		student.setName("Shivam");
+		student.setCourse("End to End Project");
+		student.setFee(2000d);
+		repo.save(student);
+	}
+
+	@Test
+	void testFindStudentById() {
+		Student student = repo.findById(1l).get();
+		System.out.println(student);
+	}
+
+	@Test
+	void testUpdateStudent() {
+		Student student = repo.findById(1l).get();
+		student.setFee(2499d);
+		repo.save(student);
+	}
+
+	@Test
+	void testDeleteStudent() {
+		Student student = repo.findById(1l).get();
+		repo.delete(student);
 	}
 
 }
