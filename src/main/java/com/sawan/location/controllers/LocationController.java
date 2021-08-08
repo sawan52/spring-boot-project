fe224e846dd4decce554aaf4232e1cca35318d9d
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-08 23:41:53 +0530

    implemented showCreate and saveLocation methods

--- src/main/java/com/sawan/location/controllers/LocationController.java
+++ src/main/java/com/sawan/location/controllers/LocationController.java
@@ -1,13 +1,30 @@
 package com.sawan.location.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sawan.location.entities.Location;
+import com.sawan.location.service.LocationService;
 
 @Controller
 public class LocationController {
 
+	@Autowired
+	private LocationService service;
+
 	@RequestMapping("/showCreate")
 	public String showCreate() {
+		return "createLocation";
+	}
+
+	@RequestMapping("/saveLoc")
+	public String saveLocation(@ModelAttribute("location") Location location, ModelMap modelMap) {
+		Location savedLocation = service.saveLocation(location);
+		String msg = "Location saved with id: " + savedLocation.getId();
+		modelMap.addAttribute("msg", msg);
 		return "createLocation";
 	}
 
