eddb583a224f91c34d125b4c77311a03eafb6524
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-08 22:34:29 +0530

    created service layer

---
+++ src/main/java/com/sawan/location/service/LocationService.java
@@ -0,0 +1,19 @@
+package com.sawan.location.service;
+
+import java.util.List;
+
+import com.sawan.location.entities.Location;
+
+public interface LocationService {
+
+	Location saveLocation(Location location);
+
+	Location updateLocation(Location location);
+
+	void deleteLocation(Location location);
+
+	Location getLocationById(int id);
+
+	List<Location> getAllLocations();
+
+}
