89070737f0aee11d0deb866f4a8b7ee4ab1db3e8
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-09 21:35:00 +0530

    implemented CRUD operation on rest location controller

--- src/main/java/com/sawan/location/controllers/LocationRESTController.java
+++ src/main/java/com/sawan/location/controllers/LocationRESTController.java
@@ -3,7 +3,12 @@ package com.sawan.location.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +25,26 @@ public class LocationRESTController {
 	@GetMapping
 	public List<Location> getAllLocations() {
 		return repository.findAll();
+	}
+
+	@PostMapping
+	public Location createLocation(@RequestBody Location location) {
+		return repository.save(location);
+	}
+
+	@PutMapping
+	public Location updateLocation(@RequestBody Location location) {
+		return repository.save(location);
+	}
+
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable("id") int id) {
+		repository.deleteById(id);
+	}
+
+	@GetMapping("/{id}")
+	public Location getLocation(@PathVariable("id") int id) {
+		return repository.findById(id).get();
 	}
 
 }
