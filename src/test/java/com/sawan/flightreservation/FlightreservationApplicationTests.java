4bf07945289ac8d2ad3d62d25d9f2612dba574ff
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-09 22:43:24 +0530

    initial commit

---
+++ src/test/java/com/sawan/flightreservation/FlightreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sawan.flightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
