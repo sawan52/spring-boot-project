30004e75e34ae05b89b1b63ef854f6be51328c07
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-09 17:33:07 +0530

    implemented GUI report generation feature using third party dependency

--- src/main/java/com/sawan/location/controllers/LocationController.java
+++ src/main/java/com/sawan/location/controllers/LocationController.java
@@ -2,6 +2,8 @@ package com.sawan.location.controllers;
 
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.sawan.location.entities.Location;
+import com.sawan.location.repos.LocationRepository;
 import com.sawan.location.service.LocationService;
 import com.sawan.location.util.EmailUtil;
+import com.sawan.location.util.ReportUtil;
 
 @Controller
 public class LocationController {
@@ -21,6 +25,15 @@ public class LocationController {
 
 	@Autowired
 	private EmailUtil emailUtil;
+
+	@Autowired
+	private LocationRepository repository;
+
+	@Autowired
+	private ReportUtil reportUtil;
+
+	@Autowired
+	private ServletContext context;
 
 	@RequestMapping("/showCreate")
 	public String showCreate() {
@@ -72,6 +85,14 @@ public class LocationController {
 		List<Location> locations = service.getAllLocations();
 		modelMap.addAttribute("locations", locations);
 		return "displayLocations";
+	}
+
+	@RequestMapping("/generateReport")
+	public String generateReport() {
+		String path = context.getRealPath("/");
+		List<Object[]> data = repository.findTypeAndTypeCount();
+		reportUtil.generatePieChart(path, data);
+		return "report";
 	}
 
 }
