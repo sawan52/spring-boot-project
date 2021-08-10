2519777bec7b815c8a451c6a7f385ab9131af788
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-10 12:28:56 +0530

    user registration and login feature added

--- src/main/java/com/sawan/flightreservation/controllers/UserController.java
+++ src/main/java/com/sawan/flightreservation/controllers/UserController.java
@@ -1,14 +1,43 @@
 package com.sawan.flightreservation.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.sawan.flightreservation.entities.User;
+import com.sawan.flightreservation.repos.UserRepository;
 
 @Controller
 public class UserController {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping("/showReg")
 	public String showRegistrationPage() {
 		return "login/registerUser";
+	}
+
+	@RequestMapping(value = "/registerUser", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user) {
+		userRepository.save(user);
+		return "login/login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginUser(@RequestParam("email") String email, @RequestParam("password") String password,
+			ModelMap modelMap) {
+		User user = userRepository.findByEmail(email);
+		if (user.getPassword().equals(password)) {
+			return "findFlights";
+		} else {
+			modelMap.addAttribute("error_msg", "Invalid username or password. Please try again!");
+		}
+		return "login/login";
 	}
 
 }
