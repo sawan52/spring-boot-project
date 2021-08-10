16488b800dedcfd4fe15799b1ec9ea9ecb52410a
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-10 17:34:56 +0530

    created reservationRestController microservice for checkedIn and numberOfBags update

---
+++ src/main/java/com/sawan/flightreservation/controllers/ReservationRestController.java
@@ -0,0 +1,34 @@
+package com.sawan.flightreservation.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sawan.flightreservation.dto.ReservationUpdateRequest;
+import com.sawan.flightreservation.entities.Reservation;
+import com.sawan.flightreservation.repos.ReservationRepository;
+
+@RestController
+@CrossOrigin
+public class ReservationRestController {
+
+	@Autowired
+	private ReservationRepository reservationRepository;
+
+	@RequestMapping("/reservations/{id}")
+	public Reservation findReservation(@PathVariable("id") Long id) {
+		return reservationRepository.findById(id).get();
+	}
+
+	@RequestMapping("/reservations")
+	public Reservation updateReservation(@RequestBody ReservationUpdateRequest request) {
+		Reservation reservation = reservationRepository.findById(request.getId()).get();
+		reservation.setNumberOfBags(request.getNumberOfBags());
+		reservation.setCheckedIn(request.getCheckedIn());
+		return reservationRepository.save(reservation);
+	}
+
+}
