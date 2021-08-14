36d4514ac8e1d75b45cea59e1423131c28ce2b9b
Sawan Singh <sawansingh5216977@gmail.com>
2021-08-14 12:41:24 +0530

    created PDFGenerator

--- src/main/java/com/sawan/flightreservation/controllers/ReservationRestController.java
+++ src/main/java/com/sawan/flightreservation/controllers/ReservationRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sawan.flightreservation.dto.ReservationUpdateRequest;
@@ -18,12 +19,12 @@ public class ReservationRestController {
 	@Autowired
 	private ReservationRepository reservationRepository;
 
-	@RequestMapping("/reservations/{id}")
-	public Reservation findReservation(@PathVariable("id") Long id) {
-		return reservationRepository.findById(id).get();
+	@RequestMapping("/reservations/{reservationId}")
+	public Reservation findReservation(@PathVariable("reservationId") Long reservationId) {
+		return reservationRepository.findById(reservationId).get();
 	}
 
-	@RequestMapping("/reservations")
+	@RequestMapping(value = "/reservations", method = RequestMethod.POST)
 	public Reservation updateReservation(@RequestBody ReservationUpdateRequest request) {
 		Reservation reservation = reservationRepository.findById(request.getId()).get();
 		reservation.setNumberOfBags(request.getNumberOfBags());
